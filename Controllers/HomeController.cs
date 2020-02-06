01fcaf3b39675c797fd2b220d51f9142f935a0f5
Lopes <daniel_slopes@hotmail.com>
2020-02-06 13:41:44 -0300

    Project started

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VirtualStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        public IActionResult CustomerRegister()
+        {
+            return View();
+        }
+    }
+}
