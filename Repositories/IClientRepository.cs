4a564bc8102991338a01f66b0faa7bb02c9d2408
Lopes <daniel_slopes@hotmail.com>
2020-02-10 13:20:31 -0300

    Add IClientRepository Interface

---
+++ Repositories/IClientRepository.cs
@@ -0,0 +1,19 @@
+﻿using VirtualStore.Models;
+using System.Collections.Generic;
+
+
+namespace VirtualStore.Repositories
+{
+    interface IClientRepository
+    {
+        Client Login(string Email, string Password);
+
+        //CRUD
+        void AddClient(Client client);
+        void UpdateClient(Client client);
+        void DeleteClient(Client client);
+        Client GetClient(int Id);
+        List<Client> GetAllClient();
+
+    }
+}
